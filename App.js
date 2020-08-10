e09dc00467fa7cde29918222ee65c3bc33a4b016
@itsAj <ajaymati1000@gmail.com>
2020-08-10 15:51:25 +0530

    added to app

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, Alert,Switch } from "react-native";
+import { StyleSheet, Text, View, Image, Alert, Switch } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
+import useAsyncStorage from "@rnhooks/async-storage";
 import {
   EvilIcons,
   Entypo,
@@ -28,44 +29,42 @@ import Product_Description_new from "./screens/product_description_new";
 import Constant from "expo-constants";
 import { AsyncStorage } from "react-native";
 import OfferDetails from "./screens/offer_details";
-import timerStart,{notify} from "./notificationManager/timer";
+import timerStart, { notify } from "./notificationManager/timer";
 import * as TaskManager from "expo-task-manager";
 import { registerForPushNotificationsAsync } from "./notificationManager";
 import * as BackgroundFetch from "expo-background-fetch";
 import { CONSTANT } from "./shared/trans";
 const NOTIFICATION = "NOTIFICATION";
-const strings= CONSTANT.app;
+const strings = CONSTANT.app;
 TaskManager.defineTask(NOTIFICATION, async () => {
-  console.log('inside manager')
+  console.log("inside manager");
   try {
     await notify();
     console.log("The task is running");
     return BackgroundFetch.Result.NewData;
-
   } catch (error) {
-    console.log('error in running task');
+    console.log("error in running task");
     return BackgroundFetch.Result.Failed;
   }
 });
 const Stack = createStackNavigator();
 class LoginCheck extends React.Component {
-  
   constructor(props) {
     super(props);
 
     this.state = {
       username: null,
       token: null,
-      lang:'en'
+      lang: "en",
     };
   }
   componentDidMount() {
     (async () => {
-      let value = await AsyncStorage.getItem('lang');
-      value = value || 'en';
+      let value = await AsyncStorage.getItem("lang");
+      value = value || "en";
       this.setState({
-        lang: value
-      })
+        lang: value,
+      });
     })();
   }
 
@@ -74,28 +73,27 @@ class LoginCheck extends React.Component {
     this._checkLocalStorage();
     this.initializeToken();
   }
-  initializeToken=async ()=>{
+  initializeToken = async () => {
     // let token = await AsyncStorage.getItem("token");
-    console.log('Token initialization called');
+    console.log("Token initialization called");
     let token;
     try {
       token = await registerForPushNotificationsAsync();
-      await AsyncStorage.setItem("token", token)
+      await AsyncStorage.setItem("token", token);
     } catch (error) {
       console.log("error while initializing token", error);
     }
 
     timerStart();
-  }
+  };
   _checkLocalStorage = async () => {
     let username = await AsyncStorage.getItem("username");
-    
-    
+
     this.setState({ username });
   };
 
   render() {
-    let lang= this.state.lang;
+    let lang = this.state.lang;
     this._checkLocalStorage();
     let BeforeLogin = (
       <View
@@ -119,7 +117,9 @@ class LoginCheck extends React.Component {
             borderColor: "#fff",
           }}
         >
-          <Text style={{ fontSize: 14, color: "#fff" }}>{strings.login[lang]}</Text>
+          <Text style={{ fontSize: 14, color: "#fff" }}>
+            {strings.login[lang]}
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => this.props.nav.navigate("LoginSignup")}
@@ -133,7 +133,9 @@ class LoginCheck extends React.Component {
             borderColor: "#fff",
           }}
         >
-          <Text style={{ fontSize: 14, color: "#fff" }}>{strings.signup[lang]}</Text>
+          <Text style={{ fontSize: 14, color: "#fff" }}>
+            {strings.signup[lang]}
+          </Text>
         </TouchableOpacity>
       </View>
     );
@@ -166,7 +168,9 @@ class LoginCheck extends React.Component {
             borderColor: "#fff",
           }}
         >
-          <Text style={{ fontSize: 14, color: "#fff" }}>{strings.logout[lang]}</Text>
+          <Text style={{ fontSize: 14, color: "#fff" }}>
+            {strings.logout[lang]}
+          </Text>
         </TouchableOpacity>
       </View>
     );
@@ -179,15 +183,15 @@ class LoginCheck extends React.Component {
 }
 
 const HomeStack = () => {
-    // useEffect
-   const [lang, setlang] = useState('en');
-   useEffect(() => {
-     (async () => {
-       let value = await AsyncStorage.getItem('lang');
-       value = value || 'en';
-       setlang(value);
-     })();
-   }, [])
+  // useEffect
+  const [lang, setlang] = useState("en");
+  useEffect(() => {
+    (async () => {
+      let value = await AsyncStorage.getItem("lang");
+      value = value || "en";
+      setlang(value);
+    })();
+  }, []);
   return (
     <Stack.Navigator>
       <Stack.Screen
@@ -262,14 +266,14 @@ const HomeStack = () => {
   );
 };
 const OfferStack = () => {
-     const [lang, setlang] = useState('en');
-     useEffect(() => {
-       (async () => {
-         let value = await AsyncStorage.getItem('lang');
-         value = value || 'en';
-         setlang(value);
-       })();
-     }, [])
+  const [lang, setlang] = useState("en");
+  useEffect(() => {
+    (async () => {
+      let value = await AsyncStorage.getItem("lang");
+      value = value || "en";
+      setlang(value);
+    })();
+  }, []);
   return (
     <Stack.Navigator>
       <Stack.Screen
@@ -300,14 +304,14 @@ const tabBarStyle = {
 
 const HomeTab = () => {
   console.log("Hometab called");
-  const [lang, setlang] = useState('en');
+  const [lang, setlang] = useState("en");
   useEffect(() => {
     (async () => {
-      let value = await AsyncStorage.getItem('lang');
-      value = value || 'en';
+      let value = await AsyncStorage.getItem("lang");
+      value = value || "en";
       setlang(value);
     })();
-  }, [])
+  }, []);
   return (
     <Tab.Navigator tabBarOptions={tabBarStyle}>
       <Tab.Screen
@@ -353,39 +357,34 @@ const HomeTab = () => {
 
 const Drawer = createDrawerNavigator();
 
-
-export default class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      lang:true
-    }
-  }
-  toggleLang = async() => {
-    console.log('toogleLang is called');
-    const isEn= this.state.lang;
-    await AsyncStorage.setItem('lang',isEn?'hi':'en');
-    console.log(isEn);
-    this.setState({
-      lang:!isEn
-    })
-    console.log(this.state.lang);
-  }
-  CustomDrawer = (props) => {
-  return (
-    <View style={{ flex: 1, backgroundColor: "#D3D3D3" }}>
-      <View
-        style={{
-          top: 0,
-          left: 0,
-          right: 0,
-          height: Constant.statusBarHeight,
-          backgroundColor: "#4CBB17",
-        }}
-      ></View>
-      <LoginCheck nav={props.navigation} />
-      {
-      /* <View style={{marginTop:5, paddingLeft:10, backgroundColor:'#fff', flexDirection:'row', height:60, alignItems:'center', justifyContent:'space-around'}}>
+export default function App() {
+  const [lang, setLang, clearLang] = useAsyncStorage("lang");
+  const [checked, setChecked] = useState(true);
+  toggleLang = async () => {
+    console.log("toogleLang is called");
+    const b = checked;
+    setChecked(!b);
+    await setLang(b ? "hi" : "en");
+    // console.log(isEn;
+    // this.setState({
+    //   lang:!isEn
+    // })
+    // console.log(this.state.lang);
+  };
+  const CustomDrawer = (props) => {
+    return (
+      <View style={{ flex: 1, backgroundColor: "#D3D3D3" }}>
+        <View
+          style={{
+            top: 0,
+            left: 0,
+            right: 0,
+            height: Constant.statusBarHeight,
+            backgroundColor: "#4CBB17",
+          }}
+        ></View>
+        <LoginCheck nav={props.navigation} />
+        {/* <View style={{marginTop:5, paddingLeft:10, backgroundColor:'#fff', flexDirection:'row', height:60, alignItems:'center', justifyContent:'space-around'}}>
         <EvilIcons name="location" size={22} color="black" />
         <Text style={{fontSize:15}}>560004, Bangalore - 560004</Text>
         <Feather name="edit-2" size={20} color="#4CBB17" style={{marginLeft:15}} />
@@ -404,75 +403,70 @@ export default class App extends React.Component {
           />
           <Text style={{fontSize:13}}>   Tomorrow 6:30AM -8:30AM</Text>
         </View>
-      </View> */
-      }
-      {/* <View style={{flexDirection:'row', height:60, backgroundColor:'#d0f0c0', justifyContent:'space-around', alignItems:'center'}}>
+      </View> */}
+        {/* <View style={{flexDirection:'row', height:60, backgroundColor:'#d0f0c0', justifyContent:'space-around', alignItems:'center'}}>
         <Text style={{fontSize:13, color:'green', width:200}}>Get Priority slots with bbstar memebership</Text>
         <AntDesign name="caretright" size={18} color="green" />
       </View> */}
-      <ScrollView
-        style={{
-          marginTop: 5,
-          paddingVertical: 10,
-          paddingHorizontal: 20,
-          backgroundColor: "#fff",
-        }}
-      >
-        <TouchableOpacity onPress={() => props.navigation.navigate("Home")}>
-          <Text style={{ fontSize: 18, marginBottom: 20 }}>घर</Text>
-        </TouchableOpacity>
+        <ScrollView
+          style={{
+            marginTop: 5,
+            paddingVertical: 10,
+            paddingHorizontal: 20,
+            backgroundColor: "#fff",
+          }}
+        >
+          <TouchableOpacity onPress={() => props.navigation.navigate("Home")}>
+            <Text style={{ fontSize: 18, marginBottom: 20 }}>घर</Text>
+          </TouchableOpacity>
 
-        <View style={ { flexDirection:'row' }}>
-          <Text style={{flex:1}}>Hindi/English</Text>
-          <Switch
-          style={{flex:1}}
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={true ? "#f5dd4b" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange = {
-          this.toggleLang
-        }
-        value={this.state.lang}
-      />
-        </View>
-        {/* <TouchableOpacity onPress={() => props.navigation.navigate('Offers')}>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={{ flex: 1 }}>Hindi/English</Text>
+            <Switch
+              style={{ flex: 1 }}
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={true ? "#f5dd4b" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={() => toggleLang()}
+              value={checked}
+            />
+          </View>
+          {/* <TouchableOpacity onPress={() => props.navigation.navigate('Offers')}>
           <Text style={{fontSize:18, marginBottom:20}}>Offers</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => props.navigation.navigate('Category')}>
           <Text style={{fontSize:18, marginBottom:20}}>Categories</Text>
         </TouchableOpacity> */}
-        {/* <TouchableOpacity onPress={() => props.navigation.navigate('Basket')}>
+          {/* <TouchableOpacity onPress={() => props.navigation.navigate('Basket')}>
           <Text style={{fontSize:18, marginBottom:20}}>Shop By Basket</Text>
         </TouchableOpacity> */}
-      </ScrollView>
-    </View>
-  );
-};
-
-  render() {
-    console.log("render called");
-    return (
-      <NavigationContainer >
-        <Drawer.Navigator
-          initialRouteName="Home"
-          // lang={this.state.lang}
-          // toggleLang= {this.toggleLang}
-          drawerContent={(props) => this.CustomDrawer(props,)}
-        >
-          <Drawer.Screen name="Home" component={HomeTab} />
-          <Drawer.Screen name="LoginSignup" component={LoginSignup} />
-          <Drawer.Screen name="Category" component={Category} />
-          <Drawer.Screen name="Offers" component={Offers} />
-          {/* <Drawer.Screen name="OfferDetails" component={OfferDetails} /> */}
-          {/* <Drawer.Screen name="Basket" component={Basket} /> */}
-        </Drawer.Navigator>
-      </NavigationContainer>
-
-      // <View style={{flex:1}}>
-      //   {/* <Home/> */}
-      //   {/* <Category/> */}
-      //   <Offers/>
-      // </View>
+        </ScrollView>
+      </View>
     );
-  }
+  };
+
+  // console.log("render called");
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        // lang={this.state.lang}
+        // toggleLang= {this.toggleLang}
+        drawerContent={(props) => CustomDrawer(props)}
+      >
+        <Drawer.Screen name="Home" component={HomeTab} />
+        <Drawer.Screen name="LoginSignup" component={LoginSignup} />
+        <Drawer.Screen name="Category" component={Category} />
+        <Drawer.Screen name="Offers" component={Offers} />
+        {/* <Drawer.Screen name="OfferDetails" component={OfferDetails} /> */}
+        {/* <Drawer.Screen name="Basket" component={Basket} /> */}
+      </Drawer.Navigator>
+    </NavigationContainer>
+
+    // <View style={{flex:1}}>
+    //   {/* <Home/> */}
+    //   {/* <Category/> */}
+    //   <Offers/>
+    // </View>
+  );
 }
